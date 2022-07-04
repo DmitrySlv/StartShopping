8a539321f9d9c47cd22c381583082b48c65f2c69
DmitrySlv <webmaestros@yandex.ru>
2022-07-04 18:54:22 +0400

    Add ShopListRepositoryImpl

---
+++ app/src/main/java/com/ds_create/startshopping/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,40 @@
+package com.ds_create.startshopping.data
+
+import com.ds_create.startshopping.domain.ShopItem
+import com.ds_create.startshopping.domain.ShopListRepository
+import java.lang.RuntimeException
+
+object ShopListRepositoryImpl: ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+    }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("Element wit id $shopItemId not found")
+    }
+
+}
