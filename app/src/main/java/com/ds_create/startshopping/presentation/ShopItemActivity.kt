eaef00ed2a8e02bb3b338b0006d63f050d1b1d54
DmitrySlv <webmaestros@yandex.ru>
2022-07-11 19:57:23 +0400

    Add LiveDatas

--- app/src/main/java/com/ds_create/startshopping/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/ds_create/startshopping/presentation/ShopItemActivity.kt
@@ -3,10 +3,16 @@ package com.ds_create.startshopping.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ds_create.startshopping.R
+import com.ds_create.startshopping.presentation.ViewModels.ShopItemViewModel
 
 class ShopItemActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: ShopItemViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+        viewModel.errorInputName.value
+        viewModel.errorInputCount.value
     }
 }
